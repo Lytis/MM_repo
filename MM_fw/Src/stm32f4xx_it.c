442f0d232e7dd8b3d01dc9f69796faf20fb41573
alexLaptop <litis.alexandros@gmail.com>
2018-09-17 12:02:32 +0300

    packet_size_fixed
    packet size fixed to 66 16bit samples

--- MM_fw/Src/stm32f4xx_it.c
+++ MM_fw/Src/stm32f4xx_it.c
@@ -348,7 +348,7 @@ void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef * hsai)
   //full_transfer_event();
 
   int i;
-  for (i=0; i<32; i++)
+  for (i=0; i<65; i++)
   {
     transmitBuffer[i+1] = (int16_t) tempBuffer[i];
   }
@@ -365,7 +365,7 @@ void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef * hsai)
     transmitBuffer[i+1] = (int16_t) tempBuffer[32+i];
   }
   HAL_GPIO_WritePin(SPI_1_EN_GPIO_Port, SPI_1_EN_Pin, GPIO_PIN_SET);
-  HAL_SPI_Transmit_DMA(&hspi1, (uint8_t*)transmitBuffer, 65);
+  HAL_SPI_Transmit_DMA(&hspi1, (uint8_t*)transmitBuffer, 66);
 }
 
 void HAL_SPI_TxHalfCpltCallback(SPI_HandleTypeDef * hspi)
